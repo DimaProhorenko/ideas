dc394eba52cc791417298772bc1e6ecb6f992b38
Dmitriy Prokhorenko <dima.study.web@gmail.com>
2024-05-05 22:22:10 +0300

    ADD basic feed layout, separate layout

---
+++ resources/views/terms.blade.php
@@ -0,0 +1,9 @@
+@extends('shared.layout')
+
+@section('content')
+    <h1>Terms</h1>
+    <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Quae voluptatibus magnam ad, consequatur distinctio sunt
+        reiciendis eveniet aperiam ex aliquam quaerat numquam a corrupti repellendus, optio nihil blanditiis? Similique,
+        perspiciatis necessitatibus, accusamus repellendus explicabo ad nulla sequi, sed nisi incidunt iste labore delectus
+        eligendi quod totam natus iusto animi unde!</p>
+@endsection
