510c28acce7d612e55ce1aecff8385c9bbc56ffc
Dmitriy Prokhorenko <dima.study.web@gmail.com>
2024-05-06 10:16:59 +0300

    ADD ideas display in feed

--- app/Http/Controllers/FeedController.php
+++ app/Http/Controllers/FeedController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Idea;
 use Illuminate\Http\Request;
 
 class FeedController extends Controller
@@ -9,6 +10,13 @@ class FeedController extends Controller
 
     public function index()
     {
-        return view('feed');
+        // $idea = new Idea([
+        //     'body' => 'Hello ideas'
+        // ]);
+        // $idea->body = 'test';
+        // $idea->save();
+        return view('feed', [
+            'ideas' => Idea::orderBy('created_at', 'DESC')->get()
+        ]);
     }
 }
